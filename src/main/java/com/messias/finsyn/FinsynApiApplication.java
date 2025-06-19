c0bf4adee0dabed486ef7af0390caab87b4a80e6
Gabriel Messias <gamessiasjt@gmail.com>
2025-06-18 21:47:08 -0300

    feat: criar projeto backend

---
+++ src/main/java/com/messias/finsyn/FinsynApiApplication.java
@@ -0,0 +1,13 @@
+package com.messias.finsyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsynApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinsynApiApplication.class, args);
+    }
+
+}
