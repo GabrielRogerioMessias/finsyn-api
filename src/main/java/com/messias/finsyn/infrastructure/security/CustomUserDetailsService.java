7b7e2d3e490c5718d1d74d6d26afabfec06b9569
Gabriel Messias <gamessiasjt@gmail.com>
2025-07-03 22:12:48 -0300

    feat: implementando UserDetailsService

---
+++ src/main/java/com/messias/finsyn/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.messias.finsyn.infrastructure.security;
+
+import com.messias.finsyn.adapters.outbounds.entities.JpaUsuarioEntity;
+import com.messias.finsyn.adapters.outbounds.repositories.JpaUsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final JpaUsuarioRepository jpaUsuarioRepository;
+
+    public CustomUserDetailsService(JpaUsuarioRepository jpaUsuarioRepository) {
+        this.jpaUsuarioRepository = jpaUsuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        JpaUsuarioEntity jpaUsuarioEntity = jpaUsuarioRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        return new org.springframework.security.core.userdetails.User(jpaUsuarioEntity.getEmail(), jpaUsuarioEntity.getSenha(), new ArrayList<>());
+    }
+}
