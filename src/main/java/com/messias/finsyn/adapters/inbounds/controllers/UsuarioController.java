5dedcb75c1f21cedeb09884be16a5fb66660e4cf
Gabriel Messias <gamessiasjt@gmail.com>
2025-07-07 21:15:26 -0300

    feat: removendo login do controlador de usuário

--- src/main/java/com/messias/finsyn/adapters/inbounds/controllers/UsuarioController.java
+++ src/main/java/com/messias/finsyn/adapters/inbounds/controllers/UsuarioController.java
@@ -1,12 +1,6 @@
 package com.messias.finsyn.adapters.inbounds.controllers;
 
-import com.messias.finsyn.adapters.inbounds.dtos.UsuarioRegistrarDTO;
 import com.messias.finsyn.application.usecases.UsuarioUseCases;
-import com.messias.finsyn.domain.models.usuario.Usuario;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +13,4 @@ public class UsuarioController {
         this.usuarioCadastroUseCase = usuarioCadastroUseCase;
     }
 
-    @PostMapping(value = "/registrar")
-    public ResponseEntity<Usuario> usuarioCadastroUseCase(@RequestBody UsuarioRegistrarDTO usuario) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCadastroUseCase.cadastrar(usuario));
-    }
 }
