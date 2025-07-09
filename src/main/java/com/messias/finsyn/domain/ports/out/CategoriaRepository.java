7fecab05a03cfbb38e0173e2a97bda169513c69c
Gabriel Messias <gamessiasjt@gmail.com>
2025-07-09 20:14:11 -0300

    feat: definindo contratos das operações da entidade categoria

---
+++ src/main/java/com/messias/finsyn/domain/ports/out/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.messias.finsyn.domain.ports.out;
+
+import com.messias.finsyn.domain.models.categoria.Categoria;
+
+import java.util.List;
+
+public interface CategoriaRepository {
+    public Categoria registrar(Categoria categoria);
+
+    public void deletarCategoria(Long idcategoria);
+
+    public List<Categoria> buscarTodas();
+
+    public Categoria atualizarCategoria(Categoria categoria, Long idCategoria);
+
+    public Categoria buscarCategoriaId(Long id);
+}
